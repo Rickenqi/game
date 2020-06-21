2feb71aa959bc381359fc643f6c4a1a8a2cbceaf
Rickenqi <rickenqi@outlook.com>
2020-06-21 22:37:44 +0800

    init

--- src/main/java/com/ricky/game/domain/LoginUser.java
+++ src/main/java/com/ricky/game/domain/LoginUser.java
@@ -1,12 +1,16 @@
 package com.ricky.game.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class LoginUser implements Serializable {
+    @JsonProperty("userId")
     Long userId;
+    @JsonProperty("userName")
     String userName;
+    @JsonProperty("userPassword")
     String userPassword;
 }
